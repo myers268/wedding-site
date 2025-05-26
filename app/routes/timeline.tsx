04f1b6e908412a74046d1b0af21dbdd836fb86ac
Zack Myers <42181342+myers268@users.noreply.github.com>
2025-05-26 18:20:16 -0400

    Stupid broken arrow

--- app/routes/timeline.tsx
+++ app/routes/timeline.tsx
@@ -1,16 +1,20 @@
 export default function Timeline() {
   return (
-    <div className="grid grid-cols-[min-content_1fr] gap-fluid-xs p-fluid-2xs">
-      <div className="py-fluid-xs h-full min-h-dvh flex flex-col items-center gap-fluid-xs">
+    <div className="grid grid-cols-[min-content_1fr_1fr] gap-fluid-xs p-fluid-2xs h-full min-h-dvh">
+      {/* <div className="py-fluid-xs h-full min-h-dvh flex flex-col items-center gap-fluid-xs"> */}
         <div
-          className="text-fluid-3xl font-extralight tracking-[0.5em] text-sage-700 text-shadow-2xs text-shadow-sage-300/50 pr-fluid-2xs"
+          className="text-fluid-3xl font-extralight tracking-[0.5em] text-sage-700 text-shadow-2xs text-shadow-sage-300/50 pr-fluid-2xs pt-fluid-sm"
           style={{ writingMode: "vertical-lr" }}
         >
           TIMELINE
         </div>
-        <div className="flex flex-col items-center flex-grow">
-          <div className="h-full min-h-fluid-2xl w-0.5 bg-sage-500 b-rounded-full"></div>
-          <div className="text-sage-500 -mt-fluid-lg">
+
+        <div className="col-span-2" />
+
+        <div className="flex flex-col items-center relative">
+          <div className="absolute w-0.5 bg-sage-500 top-0 bottom-fluid-md" />
+          <div className="flex-1" />
+          <div className="text-sage-500 sticky bottom-fluid-2xs">
             <svg
               className="size-fluid-xl"
               xmlns="http://www.w3.org/2000/svg"
@@ -27,13 +31,13 @@ export default function Timeline() {
             </svg>
           </div>
         </div>
-      </div>
+      {/* </div> */}
 
       {/* Empty first row for the right column */}
-      <div />
+      {/* <div className="col-span-2" /> */}
 
       {/* Timeline rows */}
-      <div className="text-fluid-base grid grid-cols-subgrid col-span-2 gap-fluid-xs justify-start min-h-[calc(100dvh-var(--spacing-fluid-2xs)*2)] auto-rows-min">
+      <div className="text-fluid-base grid grid-cols-subgrid col-start-2 col-span-2 gap-fluid-xs justify-start min-h-[calc(100dvh-var(--spacing-fluid-2xs)*2)] auto-rows-min">
         <div className="font-semibold font-handwritten whitespace-nowrap">
           5:30 PM
         </div>
