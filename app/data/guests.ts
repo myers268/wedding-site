0e184abd46c8ef5b66291631bf6d9ea6ea64d8b8
Zack Myers <42181342+myers268@users.noreply.github.com>
2025-06-24 11:08:55 +0700

    Update attendance

--- app/data/guests.ts
+++ app/data/guests.ts
@@ -1,5 +1,8 @@
 type Event = {
   name: string;
+  location: string;
+  timestamp: number,
+  description: string;
 }
 
 type EventAttendance = {
@@ -23,14 +26,23 @@ export type PrimaryGuest = Guest & AdditionalGuests
 
 const wedding: Event = {
   name: "Wedding",
+  location: "123 Main St, Washington, D.C. 22201",
+  timestamp: new Date(2026, 0, 2, 17, 30, 0).getTime(),
+  description: "",
 }
 
 const indiana: Event = {
   name: "Indiana Wedding Shower",
+  location: "456 Country Rd, Kokomo, IN 46902",
+  timestamp: new Date(2025, 7, 9, 14, 0, 0).getTime(),
+  description: "",
 }
 
 const washington: Event = {
   name: "Washington Wedding Shower",
+  location: "642 River Blvd, Longview, WA 99999",
+  timestamp: new Date(2025, 11, 9, 16, 0, 0).getTime(),
+  description: "",
 }
 
 export const events = [
--- app/routes/rsvp.attendance.tsx
+++ app/routes/rsvp.attendance.tsx
@@ -1,13 +1,32 @@
-import { redirect } from "react-router";
+import { redirect, useFetcher } from "react-router";
 import type { Route } from "./+types/rsvp.attendance";
 
 import { getGuestByFullName } from "#services/guests";
 import { getUserSession } from "#services/session";
+import { z } from "zod";
+
+const ATTENDING = "attending";
+const GUEST = "guest";
+const EVENT = "event";
+
+const eventSchema = z.object({
+  [ATTENDING]: z.coerce.boolean(),
+  [GUEST]: z.string(),
+  [EVENT]: z.string(),
+});
+
+export async function action({ request }: Route.ActionArgs) {
+  const formData = await request.formData();
+  const event = eventSchema.parse(Object.fromEntries(formData.entries()));
+  console.log(event)
+
+  return null;
+}
 
 export async function loader({ request }: Route.LoaderArgs) {
   // Check if user has a valid session
   const guestName = await getUserSession(request);
-  
+
   if (!guestName) {
     // Redirect to search page if no session
     throw redirect("/rsvp/search");
@@ -18,10 +37,17 @@ export async function loader({ request }: Route.LoaderArgs) {
 
   return {
     guest,
-  }
+  };
 }
 
 export default function Attendance({ loaderData }: Route.ComponentProps) {
+  const allGuests = [
+    loaderData.guest,
+    ...loaderData.guest.additionalGuests.guests,
+  ];
+
+  const eventsFetcher = useFetcher();
+
   return (
     <>
       <h2 className="text-fluid-3xl/(--spacing-fluid-3xl) font-cursive flex justify-center">
@@ -31,7 +57,10 @@ export default function Attendance({ loaderData }: Route.ComponentProps) {
         {loaderData.guest.fullName}
       </div>
       {loaderData.guest.additionalGuests.guests.map((guest) => (
-        <div key={guest.fullName} className="bg-stone-100 border-2 border-double w-full max-w-[50ch] p-fluid-sm font-handwritten">
+        <div
+          key={guest.fullName}
+          className="bg-stone-100 border-2 border-double w-full max-w-[50ch] p-fluid-sm font-handwritten"
+        >
           {guest.fullName}
         </div>
       ))}
@@ -39,8 +68,43 @@ export default function Attendance({ loaderData }: Route.ComponentProps) {
         Events
       </h2>
       {loaderData.guest.attendance.map((attendance) => (
-        <div key={attendance.event.name} className="">
-
+        <div
+          key={attendance.event.name}
+          className="grid gap-fluid-sm bg-stone-100 border-2 border-double w-full max-w-[50ch] p-fluid-sm font-handwritten"
+        >
+          <div>
+            <h3 className="text-fluid-xl">{attendance.event.name}</h3>
+            <div>{attendance.event.location}</div>
+            <div>{new Date(attendance.event.timestamp).toLocaleString()}</div>
+          </div>
+          <ul>
+            {allGuests.map((guest) => (
+              <li key={guest.fullName}>
+                <eventsFetcher.Form
+                  method="POST"
+                  onChange={(event) => {
+                    eventsFetcher.submit(event.currentTarget);
+                  }}
+                >
+                  <label className="flex items-center gap-fluid-xs select-none">
+                    <input
+                      className="accent-sage-500 size-fluid-sm"
+                      type="checkbox"
+                      name={ATTENDING}
+                      defaultChecked={
+                        guest.attendance.find(
+                          (x) => x.event.name === attendance.event.name
+                        )?.attending
+                      }
+                    />
+                    {guest.fullName}
+                  </label>
+                  <input className="hidden" aria-hidden readOnly name={GUEST} value={guest.fullName} />
+                  <input className="hidden" aria-hidden readOnly name={EVENT} value={attendance.event.location} />
+                </eventsFetcher.Form>
+              </li>
+            ))}
+          </ul>
         </div>
       ))}
     </>
