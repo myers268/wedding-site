1aadbcb34ceee3625132eab28e7e66eb12d80e4a
Zack Myers <42181342+myers268@users.noreply.github.com>
2025-07-21 20:26:23 -0400

    Adjust about spacing

--- app/routes/about.tsx
+++ app/routes/about.tsx
@@ -63,20 +63,20 @@ export default function About() {
         <h2 className="font-cursive text-fluid-2xl leading-fluid-3xl text-center">
           The Wedding Party
         </h2>
-        <div className="grid grid-cols-3 @max-[60rem]:max-w-[50ch] mx-auto">
+        <div className="grid grid-cols-3 p-fluid-xs max-w-[100ch] mx-auto text-lg">
           {col.map((x) => {
             return (
               <div
                 key={x.id}
-                className="group col-span-3 grid grid-cols-subgrid grid-rows-2 *:row-span-2 @max-[60rem]:*:row-span-1 grid-flow-col p-fluid-xs gap-fluid-xs"
+                className="group col-span-3 grid grid-cols-subgrid grid-rows-[min-content_1fr] *:row-span-2 @max-[70rem]:*:row-span-1 grid-flow-col py-fluid-xs gap-fluid-xs"
               >
-                <div className="frame before:bg-white group-even:-col-end-1 @max-[60rem]:col-span-3 drop-shadow-sm drop-shadow-sage-700/20">
+                <div className="frame before:bg-white group-even:-col-end-1 @max-[70rem]:col-span-3 drop-shadow-sm drop-shadow-sage-700/20 @max-[70rem]:max-w-[30ch] mx-auto">
                   <img
                     src="/mirage.jpg"
                     className="aspect-square object-cover"
                   />
                 </div>
-                <div className="col-span-2 @max-[60rem]:col-span-3">
+                <div className="col-span-2 @max-[70rem]:col-span-3 @max-[70rem]:max-w-[70ch] mx-auto">
                   Lorem ipsum dolor sit amet consectetur adipiscing elit.
                   Quisque faucibus ex sapien vitae pellentesque sem placerat. In
                   id cursus mi pretium tellus duis convallis. Tempus leo eu
